3f0f89abc84b98a46994570ec44c1de2aa3fafa6
makwanaavi <makwanaavi73@gmail.com>
2025-06-12 16:31:31 +0530

    first commit

---
+++ app/(users)/about/page.jsx
@@ -0,0 +1,5 @@
+const About = async () => {
+  return <h1 className="text-pink-300">Hello About</h1>;
+};
+
+export default About;
---
+++ app/(users)/service/page.jsx
@@ -0,0 +1,8 @@
+import style from'./service.module.css'
+
+const Services = async () => {
+  return <h1 className={style["h1-heading"]}>Hello Services</h1>;
+
+};
+
+export default Services;
